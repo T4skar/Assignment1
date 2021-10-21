99327bfa37c1cc21cce68d4e5a2255202bed1009
laia <laiapachon02@gmail.com>
2021-10-21 22:10:30 +0200

    spikes

---
+++ the_haunted_cave/the_haunted_cave/Output/Assets/maps/Spikes.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Spikes" tilewidth="128" tileheight="128" spacing="12" tilecount="1" columns="1">
+ <image source="../../../../../assets/Tiles/PNG/Platformer/Spikes.png" width="128" height="128"/>
+</tileset>
