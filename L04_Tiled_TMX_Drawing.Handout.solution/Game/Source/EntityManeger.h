65b420e00772bc3bcb449c2ffd27067c3a2a7480
T4skar <79161107+T4skar@users.noreply.github.com>
2022-01-30 00:08:37 +0100

    Sisisi system

--- L04_Tiled_TMX_Drawing.Handout.solution/Game/Source/EntityManeger.h
+++ L04_Tiled_TMX_Drawing.Handout.solution/Game/Source/EntityManeger.h
@@ -1,61 +1,61 @@
-#ifndef __ENTIYMANEGER_H__
-#define __ENTIYMANEGER_H__
-
-
-#include "Module.h"
-#include "Entity.h"
-#include "List.h"
-
-#define MAX_ENTITIES 100
-
-class Entity;
-
-class EntityManager : public Module
-{
-public:
-	EntityManager();
-
-	virtual ~EntityManager();
-	void Init();
-
-
-	bool Awake(pugi::xml_node&);
-
-
-	bool Start();
-
-
-	bool Update(float dt);
-
-
-	bool PostUpdate();
-
-
-	bool CleanUp();
-
-
-	Entity* CreateEntity(int x, int y, EntityType type, Entity* playerPointer = nullptr, EnemyType eType = EnemyType::NO_TYPE);
-	void DestroyEntity(Entity* entity);
-
-
-	bool UpdateAll(float dt, bool doLogic);
-
-
-	void OnCollision(Collider* c1, Collider* c2);
-
-
-
-
-	bool Load(pugi::xml_node&);
-	bool Save(pugi::xml_node&);
-
-public:
-	List<Entity*> entities;
-
-	bool doLogic = false;
-
-	int enemyDestroyedFx = 0;
-
-}
-
-#endif //!__MODULE_PLAYER_H__ 
+//#ifndef __ENTIYMANEGER_H__
+//#define __ENTIYMANEGER_H__
+//
+//
+//#include "Module.h"
+//#include "Entity.h"
+//#include "List.h"
+//
+//#define MAX_ENTITIES 100
+//
+//class Entity;
+//
+//class EntityManager : public Module
+//{
+//public:
+//	EntityManager();
+//
+//	virtual ~EntityManager();
+//	void Init();
+//
+//
+//	bool Awake(pugi::xml_node&);
+//
+//
+//	bool Start();
+//
+//
+//	bool Update(float dt);
+//
+//
+//	bool PostUpdate();
+//
+//
+//	bool CleanUp();
+//
+//
+//	Entity* CreateEntity(int x, int y, EntityType type, Entity* playerPointer = nullptr, EnemyType eType = EnemyType::NO_TYPE);
+//	void DestroyEntity(Entity* entity);
+//
+//
+//	bool UpdateAll(float dt, bool doLogic);
+//
+//
+//	void OnCollision(Collider* c1, Collider* c2);
+//
+//
+//
+//
+//	bool Load(pugi::xml_node&);
+//	bool Save(pugi::xml_node&);
+//
+//public:
+//	List<Entity*> entities;
+//
+//	bool doLogic = false;
+//
+//	int enemyDestroyedFx = 0;
+//
+//}
+//
+//#endif //!__MODULE_PLAYER_H__ 
