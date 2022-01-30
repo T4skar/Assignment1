62f12a756e68db48441400260b5f88700f306f67
T4skar <79161107+T4skar@users.noreply.github.com>
2022-01-30 22:00:29 +0100

    FULL screen HD

--- L04_Tiled_TMX_Drawing.Handout.solution/Game/Source/Window.cpp
+++ L04_Tiled_TMX_Drawing.Handout.solution/Game/Source/Window.cpp
@@ -112,3 +112,15 @@ int Window::GetHeight() const
 	return (int)height;
 }
 
+void Window::SwitchScreen()
+{
+	if (fullScreenWindow == true)
+	{
+		SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+	}
+	else
+	{
+		SDL_SetWindowFullscreen(window, 0);
+	}
+
+}
